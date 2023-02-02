98bcb6b3b581a187b5050847975bd08b22465546
Martina <martina93fernandez@hotmail.com>
2023-02-02 17:50:07 -0300

    ultimo commit

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 console.log ('hello world'); 
 let nombre= prompt("dime tu nombre");
 alert("Hola " + nombre)
+
+function miFuncion() {
+  document.getElementById("metodo").innerHTML = "basandome en la corriente conductista busco modificar el comportamiento del paciente utilizando como base el metodo de la observacion directa del paciente para su analisis y posterior correccion de conductas equivocas.";
+}
